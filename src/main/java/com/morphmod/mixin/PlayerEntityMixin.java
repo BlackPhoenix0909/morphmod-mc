8c2fad609c746cec92dc3444f27147954e542588
BlackPhoenix0909 <juliuscguhl@icloud.com>
2026-05-29 13:23:42 +0200

    Update PlayerEntityMixin.java

--- src/main/java/com/morphmod/mixin/PlayerEntityMixin.java
+++ src/main/java/com/morphmod/mixin/PlayerEntityMixin.java
@@ -3,6 +3,7 @@ package com.morphmod.mixin;
 import com.morphmod.MorphData;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.damage.DamageSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,10 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin {
 
-    /**
-     * Spider / Cave Spider: allow wall climbing.
-     */
-    @Inject(method = "isClimbing", at = @At("RETURN"), cancellable = true)
+    @Inject(method = "isClimbing()Z", at = @At("RETURN"), cancellable = true)
     private void morphmod_isClimbing(CallbackInfoReturnable<Boolean> cir) {
         PlayerEntity self = (PlayerEntity)(Object)this;
         MorphData.getActiveMorphType(self.getUuid()).ifPresent(type -> {
@@ -26,17 +24,15 @@ public abstract class PlayerEntityMixin {
         });
     }
 
-    /**
-     * Chicken: slow fall.
-     */
-    @Inject(method = "handleFallDamage", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "handleFallDamage(FFLnet/minecraft/entity/damage/DamageSource;)Z",
+            at = @At("HEAD"), cancellable = true)
     private void morphmod_handleFallDamage(float fallDistance, float damageMultiplier,
-                                            net.minecraft.entity.damage.DamageSource source,
+                                            DamageSource source,
                                             CallbackInfoReturnable<Boolean> cir) {
         PlayerEntity self = (PlayerEntity)(Object)this;
         MorphData.getActiveMorphType(self.getUuid()).ifPresent(type -> {
             if (type == EntityType.CHICKEN || type == EntityType.PARROT) {
-                cir.setReturnValue(false); // No fall damage
+                cir.setReturnValue(false);
             }
         });
     }
